43544b45d4c275fdd629a363116c4ccc5986a0a0
Dana Sugu <dana.sugu@gmail.com>
2021-07-27 19:45:35 +0300

    Update main.blade.php

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -58,10 +58,10 @@
                 </a>
                 <div id="collapseSystem" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="py-2 bg-white rounded collapse-inner">
-                        <a class="collapse-item" href="{{ route('countries.index') }}">Country</a>
-                        <a class="collapse-item" href="{{ route('states.index') }}">State</a>
-                        <a class="collapse-item" href="{{ route('departments.index') }}">Department</a>
-                        <a class="collapse-item" href="{{ route('cities.index') }}">City</a>
+                        <a class="collapse-item" href="">Country</a>
+                        <a class="collapse-item" href="">State</a>
+                        <a class="collapse-item" href="">Department</a>
+                        <a class="collapse-item" href="">City</a>
                     </div>
                 </div>
             </li>
