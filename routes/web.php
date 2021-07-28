d8d7b4c78541ceb23a9b10bb573a5e88156ca4ae
Dana Sugu <dana.sugu@gmail.com>
2021-07-28 08:02:00 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::middleware(('auth:sancum','verified'))->group(function() {
+    Route::view('dashboard', 'dashboard')->name('dashboard');
+});
