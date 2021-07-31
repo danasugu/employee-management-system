c2ce04ef8edd223451d5ff774c36adf954157627
Dana Sugu <dana.sugu@gmail.com>
2021-07-31 09:16:21 +0300

    Update user-index.blade.php

--- resources/views/livewire/users/user-index.blade.php
+++ resources/views/livewire/users/user-index.blade.php
@@ -87,7 +87,7 @@
 
                                 <div class="col-md-6">
                                     <input id="username" type="text"
-                                        class="form-control @error('name') is-invalid @enderror" name="username"
+                                        class="form-control @error('name') is-invalid @enderror" wire:model="username"
                                         value="{{ old('username') }}" required autocomplete="username" autofocus>
 
                                     @error('username')
