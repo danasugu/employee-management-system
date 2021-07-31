c8376773cea4f7ef071b82c07b7e024b78dda19d
Dana Sugu <dana.sugu@gmail.com>
2021-07-31 08:57:19 +0300

    Update user-index.blade.php

--- resources/views/livewire/users/user-index.blade.php
+++ resources/views/livewire/users/user-index.blade.php
@@ -33,7 +33,10 @@
                         </form>
                     </div>
                     <div>
-                        <a href="" class="mb-2 btn btn-primary">Create</a>
+                        {{-- <a href="" class="mb-2 btn btn-primary">Create</a> --}}
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                        Launch demo modal
+                        </button>
                     </div>
                 </div>
             </div>
